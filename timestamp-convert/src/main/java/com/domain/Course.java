ae7888657aa888e74648fc3e37f7c422ec035e57
squirrelzt <squirrelzt@126.com>
2019-09-06 18:11:44 +0800

    @NotEmpty用在集合类上面,@NotBlank 用在String上面,@NotNull用在基本类型上

--- timestamp-convert/src/main/java/com/domain/Course.java
+++ timestamp-convert/src/main/java/com/domain/Course.java
@@ -2,14 +2,32 @@ package com.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class Course {
 
+    /**
+     * 用在基本类型上
+     */
+    @NotNull(message = "id 不能为空")
     private long id;
 
+    /**
+     * 用在String上面
+     */
+    @NotBlank(message = "姓名不能为空")
     private String name;
+
+    /**
+     * 用在集合类上面
+     */
+    @NotEmpty (message = "地址不能为空")
+    private List<String> content;
 
     private Date createDate;
 
