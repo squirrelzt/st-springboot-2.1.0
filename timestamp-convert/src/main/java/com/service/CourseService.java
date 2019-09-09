dba39d39e637a8a4b143ede14f09045ce23eb2d2
squirrelzt <squirrelzt@126.com>
2019-09-09 15:37:02 +0800

    时间戳备注

--- timestamp-convert/src/main/java/com/service/CourseService.java
+++ timestamp-convert/src/main/java/com/service/CourseService.java
@@ -5,7 +5,18 @@ import com.domain.Course;
 import java.text.ParseException;
 import java.util.List;
 
+/**
+ * 类名称: CourseService
+ * 类描述: 课程服务接口
+ * @author squirrel
+ * @date 2019-09-09
+ */
 public interface CourseService {
 
+    /**
+     *
+     * @return
+     * @throws ParseException
+     */
     List<Course> listCourses() throws ParseException;
 }
