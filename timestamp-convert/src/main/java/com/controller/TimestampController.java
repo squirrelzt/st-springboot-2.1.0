96338c0749289dbfcc4dc12abd84aa6f3d4019d4
squirrelzt <squirrelzt@126.com>
2019-09-09 13:29:11 +0800

    时间戳备注

--- timestamp-convert/src/main/java/com/controller/TimestampController.java
+++ timestamp-convert/src/main/java/com/controller/TimestampController.java
@@ -11,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.text.ParseException;
 import java.util.List;
 
+/**
+ * 类名称: TimestampController
+ * 类描述: 时间戳转换控制器
+ * @author squirrel
+ * @date 2019-09-09
+ */
 @Slf4j
 @RestController
 @RequestMapping("/timestampConvert")
@@ -22,9 +28,14 @@ public class TimestampController {
         this.courseService = courseService;
     }
 
+    /**
+     * 查询所有课程
+     * @param id 序号
+     * @return 课程集合
+     */
     @PostMapping("/course")
     public List<Course> listCourses(@RequestParam("id") Long id) {
-        System.out.println(id);
+        log.info("入参: {}", id);
         try {
             return courseService.listCourses();
         } catch (ParseException e) {
