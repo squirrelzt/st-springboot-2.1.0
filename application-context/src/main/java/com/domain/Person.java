a9a3bfc4f631542756bda7d540669cdc4fe4811f
squirrelzt <squirrelzt@126.com>
2019-09-23 15:46:07 +0800

    lombok添加无参构造方法和所有参数构造方法

--- application-context/src/main/java/com/domain/Person.java
+++ application-context/src/main/java/com/domain/Person.java
@@ -1,8 +1,12 @@
 package com.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     private String name;
     private Integer age;
