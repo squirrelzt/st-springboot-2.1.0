b7a0f91716991f3f4e4f66836c1820c35235ab68
squirrelzt <squirrelzt@126.com>
2019-10-10 10:39:33 +0800

    spring security

---
+++ st-security/src/main/java/com/security/SecurityApplicationMain.java
@@ -0,0 +1,12 @@
+package com.security;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SecurityApplicationMain {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SecurityApplicationMain.class).run();
+    }
+}
