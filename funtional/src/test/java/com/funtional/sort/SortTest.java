b19e0d2edc7a2b36b94b5defbb4858486dbc6091
squirrelzt <squirrelzt@126.com>
2020-11-22 19:04:49 +0800

    sorted()

---
+++ funtional/src/test/java/com/funtional/sort/SortTest.java
@@ -0,0 +1,20 @@
+package com.funtional.sort;
+
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertEquals;
+
+public class SortTest {
+
+    @Test
+    public void givenCollections_withStream_thenSort() {
+        List<String> list = Arrays.asList("Beijing", "Shagnhai", "Hebei");
+        List<String> sortedList = list.stream().sorted().collect(toList());
+        assertEquals("Beijing", sortedList.get(0));
+    }
+}
